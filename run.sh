cd131283e17d3f086c4c39d97086f5f31d8f80a7
Chris Bearman <chris@chrisjb.com>
2019-10-18 18:54:46 -0500

    Use docker

--- run.sh
+++ run.sh
@@ -7,18 +7,13 @@ echo "3. Have WIFI provisioned for your network"
 echo "4. Enable multicast DNS to make devices easy to find"
 echo ""
 
-if [ "$EUID" -ne 0 ]; then
-	echo "Please run as root (sudo ./run.sh)"
-	exit 1
-fi
-
-
+rm -f config
 AUTH_KEYS=$HOME/.ssh/authorized_keys
 if [ -f $HOME/.ssh/authorized_keys ]; then
 	while true; do
     		read -p "Do you wish to install $AUTH_KEYS? " yn
     		case $yn in
-			[Yy]* ) export INSTALL_SSH_AUTHORIZED_KEYS=$AUTH_KEYS; break;;
+			[Yy]* ) echo "INSTALL_SSH_AUTHORIZED_KEYS=$AUTH_KEYS" >> config; break;;
         		[Nn]* ) echo "Okay, will not install $AUTH_KEYS"; break;;
         		* ) echo "Please answer yes or no.";;
     		esac
@@ -30,7 +25,7 @@ fi
 while [ -z "$FIRST_USER_NAME" ]; do
 	read -p "Please enter name of login user: " FIRST_USER_NAME
 done
-export FIRST_USER_NAME
+echo "FIRST_USER_NAME=${FIRST_USER_NAME}" >> config
 
 while [ -z "$FIRST_USER_PASS" ]; do
 	read -s -p "Please enter password of login user   : " FIRST_USER_PASS
@@ -43,17 +38,16 @@ while [ -z "$FIRST_USER_PASS" ]; do
 		echo "Passwords do not match, please try again"
 	fi
 done
-export FIRST_USER_PASS
+echo "FIRST_USER_PASS=${FIRST_USER_PASS}" >> config
 
 read -p "Enter SSID of wifi network, leave empty for no wifi config: " WIFI_NET
 if [ ! -z $WIFI_NET ]; then
-	export WPA_ESSID=$WIFI_NET
+	echo "WPA_ESSID=${WIFI_NET}" >> config
 	while [ -z "$WPA_PASSWORD" ]; do
 		read -s -p "Please enter wifi password: " WPA_PASSWORD
 		echo ""
 	done
-	export WPA_PASSWORD
-	export WPA_COUNTRY=US
+	echo "WPA_COUNTRY=US" >> config
 else 
 	echo "Wifi will not be enabled"
 fi
@@ -61,13 +55,13 @@ fi
 while [ -z $IMG_NAME ]; do
 	read -p "Image name: " IMG_NAME
 done
-export IMG_NAME
+echo "IMG_NAME=${IMG_NAME}" >> config
 
 while true; do
 	read -p "Do you wish to build all images (yes) or just lite (no): " yn
 	case $yn in
-		[Yy]* ) BUILD_ALL=1; break;;
-		[Nn]* ) BUILD_ALL=0; break;;
+		[Yy]* ) echo "BUILD_ALL=1" >> config; break;;
+		[Nn]* ) echo "BUILD_ALL=0" >> config; break;;
 		* ) echo "Please answer yes or no.";;
 	esac
 done
@@ -81,8 +75,8 @@ else
 	echo "Building all images"
 fi
 
-export ENABLE_SSH=1
-export CLEAN=1
+echo "ENABLE_SSH=1" >> config
+echo "CLEAN=1" >> config
 
-apt-get install -y coreutils quilt parted qemu-user-static debootstrap zerofree pxz zip dosfstools bsdtar libcap2-bin grep rsync xz-utils file git curl
-./build.sh
+sudo apt-get install -y coreutils quilt parted qemu-user-static debootstrap zerofree pxz zip dosfstools bsdtar libcap2-bin grep rsync xz-utils file git curl
+./build-docker.sh
